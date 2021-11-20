307e3934bb5fc1d95628bfb4b03c157bfde11c44
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-20 17:47:17 +0200

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PartyInvites.Entity;
+using PartyInvites.Model;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController: Controller
+    {
+        private DataContext context;
+
+        public HomeController(DataContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Responde()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Responde(GuestResponse guestResponde)
+        {
+            context.Add(guestResponde);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Thanks), 
+                new { Name = guestResponde.Name, WillAttend = guestResponde.WiilAttend});
+        }
+
+        public IActionResult Thanks(GuestResponse response) => View(response);
+
+        public IActionResult ListResponse() =>
+            View(context.Response.OrderByDescending(r => r.WiilAttend));
+
+    }
+}
---
+++ PartyInvites/Entity/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PartyInvites.Model;
+
+namespace PartyInvites.Entity
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<GuestResponse> Response { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            :base (options)
+        { 
+        }
+    }
+}
